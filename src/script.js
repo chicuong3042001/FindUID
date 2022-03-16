f43ee2d8c864b7f5f6c2823202dda2de3b142e62
chicuong3042001 <chicuong3042001@gmail.com>
2022-03-16 19:12:04 +0700

    update

--- src/script.js
+++ src/script.js
@@ -3,35 +3,39 @@ function showFindUID(_this){
     var contentMain = document.getElementById('content__main');
     contentMain.innerHTML = htmlFindUID;
     setColor();
-    _this.style.backgroundColor = "rgb(211, 206, 206)";
+    _this.style.backgroundColor = "rgb(58, 89, 170)";
+    _this.style.color = "white";
 }
 
 function showFindFBfromEmail(_this){
     var contentMain = document.getElementById('content__main');
     contentMain.innerHTML = htmlFindFBfromEmail;
     setColor();
-    _this.style.backgroundColor = "rgb(211, 206, 206)";
-    
+    _this.style.backgroundColor = "rgb(58, 89, 170)";
+    _this.style.color = "white";
 }
 
 function showFindFBfromPhone(_this){
     var contentMain = document.getElementById('content__main');
     contentMain.innerHTML = htmlFindFBfromPhone;
     setColor();
-    _this.style.backgroundColor = "rgb(211, 206, 206)";
+    _this.style.backgroundColor = "rgb(58, 89, 170)";
+    _this.style.color = "white";
 }
 
 function setColor(){
     var allBtnColor = document.getElementsByClassName('btn');
     for(let btnColor of allBtnColor){
-        btnColor.style.backgroundColor = "white";
+        btnColor.style.backgroundColor = "#e5e5e5";
+        btnColor.style.color = "black"
     }
 }
 
 window.onload = function(){ 
     setColor();
     let btnFindUID = document.getElementById('btnFindUID');
-    btnFindUID.style.backgroundColor = "rgb(211, 206, 206)";
+    btnFindUID.style.backgroundColor = "rgb(58, 89, 170)";
+    btnFindUID.style.color = "white";
 }
 
 let htmlFindUID = `
@@ -52,13 +56,28 @@ let htmlFindUID = `
 
 let htmlFindFBfromPhone = `
 <h3>Tìm Facebook từ số điện thoại</h3>
-<p>Bằng cách nhập dữ liệu trên hệ thống tìm kiếm của chúng tôi, bạn đã đồng ý với <a style="text-decoration: none;color: rgb(32, 32, 250);" href="#">Điều khoản sử dụng</a> và <a style="text-decoration: none;color:rgb(32, 32, 250);" href="#">Chính sách bảo mật</a> của chúng tôi.</p>
+<p>Bằng cách nhập dữ liệu trên hệ thống tìm kiếm của chúng tôi, bạn đã đồng ý với 
+<a style="text-decoration: none;color: rgb(32, 32, 250);" href="#">Điều khoản sử dụng</a> và 
+<a style="text-decoration: none;color:rgb(32, 32, 250);" href="#">Chính sách bảo mật</a> của
+chúng tôi.</p>
 <form>
     <input type="text" placeholder="Nhập số điện thoại vào đây...">
-    <button class="btnSubmit"><img style="width:20px;margin-right: 5px;"src="https://img.icons8.com/external-colours-bomsymbols-/91/000000/external-find-digital-design-colors-set-1-colours-bomsymbols--2.png"/>Tìm Facebook</button>
-    <p class="message">Kết quả sẽ xuất hiện tại đây!</p>
+    <button class="btnSubmit"><img style="width:20px;margin-right: 5px;"
+    src="https://img.icons8.com/external-colours-bomsymbols-/91/000000/external-find-digital-design-colors-set-1-colours-bomsymbols--2.png" />Tìm
+    Facebook</button>
+<p class="message">Kết quả sẽ xuất hiện tại đây!</p>
 </form>
-<p class="CountCheck">Số lượt check còn lại trong ngày: 30</p>
+<p class="CountCheck">Số lượt check còn lại trong ngày:  <strong style="color:red">30</strong></p>
+<div class="content__four-ways">
+<hr>
+<h3 style="text-decoration:underline; margin-top:35px">*Mẹo khai thác số điện thoại hiệu quả</h3>
+<div class="four_ways">
+    <a href="#">1. Cách tìm Facebook nhanh bằng SĐT và lấy ID Facebook nhanh chóng tại finduid.com</a>
+    <a href="#">2. Hướng dẫn kết bạn full 2k bạn không cần SĐT</a>
+    <a href="#">3. Vì sao nên bán hàng trên ZALO? Làm thế nào để full 2k bạn ZALO?</a>
+    <a href="#">4. Kinh nghiệm bán TRĂM ĐƠN HÀNG online trên ZALO cá nhân - Bí mật chưa được tiết lộ</a>
+</div>
+</div>
 `
 let htmlFindFBfromEmail = `
 <h3>Tìm Facebook từ Email</h3>
@@ -68,5 +87,5 @@ let htmlFindFBfromEmail = `
     <button class="btnSubmit"><img style="width:20px;margin-right: 5px;"src="https://img.icons8.com/external-colours-bomsymbols-/91/000000/external-find-digital-design-colors-set-1-colours-bomsymbols--2.png"/>Tìm Facebook</button>
     <p class="message">Kết quả sẽ xuất hiện tại đây!</p>
 </form>
-<p class="CountCheck">Số lượt check còn lại trong ngày: 30</p>
+<p class="CountCheck">Số lượt check còn lại trong ngày: <strong style="color:red">30</strong></p>
 `
